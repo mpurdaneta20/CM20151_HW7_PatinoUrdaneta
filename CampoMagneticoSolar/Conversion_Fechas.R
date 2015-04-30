8f00f9714ed480d472152860298b52c9a4c30075
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2015-04-30 18:42:29 -0500

    conversion fechas a serie de tiempo

---
+++ CampoMagneticoSolar/Conversion_Fechas.R
@@ -0,0 +1,20 @@
+library(lubridate)
+library(dplyr)
+library(tidyr)
+times <- read.csv("times.csv")
+
+colnames(times) <- c("Fecha","Hora")
+times <- data.frame(do.call(rbind, strsplit(as.vector(times$Hora), split = "_")))
+times$FechaHora <- paste(times$X1, times$X2, sep=" ")
+
+Fechas <- subset(times, select = FechaHora)
+
+Fechas$FechaHora <- strptime(as.character(Fechas$FechaHora), format = "%Y.%m.%d %H:%M:%S")
+time_series = vector(mode="integer", length=207)
+
+for (i in 1:207 ) {
+  time_series[i] <- difftime(Fechas$FechaHora[i], Fechas$FechaHora[1], units = "mins") 
+}
+
+write.csv(time_series, file = "time_series.csv", row.names = FALSE)
+
